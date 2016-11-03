673f5c5d9ffde941a0002155a0e6ffd4cc05f525
Bernabe Gonzalez <bernabe@toptierlabs.com>
2016-11-03 19:08:10 -0300

    session added and exteded api service

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,5 @@
 export const LOAD_USERS_SUCCESS = 'LOAD_USERS_SUCCESS';
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
---
+++ src/services/sessionService.js
@@ -0,0 +1,43 @@
+import * as constant from '../constants/apiConstants';
+import loginPatch from '../utils/loginPatch';
+
+export const loadSession = () => {
+  try {
+    const serializedState = localStorage.getItem(constant.SECCION_STORAGE);
+    if (serializedState === null) {
+      localStorage.setItem('stub', 'stub');
+      localStorage.removeItem('stub');
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    let session = new loginPatch();
+    return session.getSession();
+  }
+};
+
+export const saveSession = (token) => {
+  try {
+    const serializedState = JSON.stringify(token);
+    localStorage.setItem(constant.SECCION_STORAGE, serializedState);
+  } catch (e) {
+    let session = new loginPatch();
+    session.setSession(token.id, token.token, token.email);
+  }
+};
+
+export const deleteSession = (key = constant.SECCION_STORAGE) => {
+  try {
+    localStorage.removeItem(key);
+    localStorage.setItem('stub', 'stub');
+    localStorage.removeItem('stub');
+  } catch (e) {
+    let session = new loginPatch();
+    session.deleteSession();
+  }
+};
+
+export const isLogged = () => {
+  let currentSession = loadSession();
+  return (currentSession && currentSession.email && currentSession.token);
+};
